7cb011b5be7f5ee545b35307ad7a3795ca52bc70
LaalShaitaan <spalriwalau@gmail.com>
2020-10-25 15:14:37 +0530

    Search now shows Happy+Other Articles

--- controllers/controller.js
+++ controllers/controller.js
@@ -41,45 +41,6 @@ const positiveNews = (req, res) => {
     })
 }
 
-const searchNews = (req, res) => {
-    const topic=req.body.topic;
-    axios.get('https://newsapi.org/v2/everything?q='+topic+'&sortBy=publishedAt&language=en&apiKey='+process.env.NEWS_API_KEY)
-    .then(response => {
-        const sentiment = new Sentiment()
-        
-        const positiveArticles = response.data.articles.filter(article => {
-            for (let field in article) {
-                if (field === 'title' && article[field] === null ||
-                    field === 'description' && article[field] === null ||
-                    field === 'url' && article[field] === null ||
-                    field === 'urlToImage' && article[field] === null
-                ) {
-                    return false
-                }
-            }
-
-            const content = article.title + " " + article.description
-            const result = sentiment.analyze(content)
-            return result.score > 1
-        })
-
-        const positiveArticlesUnique = positiveArticles.filter((article, index) => {
-            return positiveArticles.map(articleObj => articleObj.title).indexOf(article.title) === index
-        })
-        
-
-        res.status(200).json({
-            data: {
-                articles: positiveArticlesUnique
-            }
-
-        })
-    })
-    .catch(error => {
-        res.status(500).send(error);
-    })
-}
-
 const allNews= (req,res)=>{
     axios.get(`https://newsapi.org/v2/top-headlines?country=in&language=en&sortBy=publishedAt&pageSize=16&apiKey=${process.env.NEWS_API_KEY}`)
     .then(response => {
@@ -111,6 +72,64 @@ const allNews= (req,res)=>{
         res.status(500).send(error);
     })
 };
+
+const searchNews = (req, res) => {
+    const topic=req.body.topic;
+    axios.get('https://newsapi.org/v2/everything?q='+topic+'&sortBy=publishedAt&language=en&apiKey='+process.env.NEWS_API_KEY)
+    .then(response => {
+        const sentiment = new Sentiment()
+        
+        const positiveArticles = response.data.articles.filter(article => {
+            for (let field in article) {
+                if (field === 'title' && article[field] === null ||
+                    field === 'description' && article[field] === null ||
+                    field === 'url' && article[field] === null ||
+                    field === 'urlToImage' && article[field] === null
+                ) {
+                    return false
+                }
+            }
+
+            const content = article.title + " " + article.description
+            const result = sentiment.analyze(content)
+            return result.score > 1
+        })
+
+        const positiveArticlesUnique = positiveArticles.filter((article, index) => {
+            return positiveArticles.map(articleObj => articleObj.title).indexOf(article.title) === index
+        })
+
+        const allArticles = response.data.articles.filter(article => {
+            for (let field in article) {
+                if (field === 'title' && article[field] === null ||
+                    field === 'description' && article[field] === null ||
+                    field === 'url' && article[field] === null ||
+                    field === 'urlToImage' && article[field] === null
+                ) {
+                    return false
+                }
+            }
+
+            const content = article.title + " " + article.description
+            return content
+        })
+
+        const uniqueArticles = allArticles.filter((article, index) => {
+            return allArticles.map(articleObj => articleObj.title).indexOf(article.title) === index
+        })
+
+        
+
+        res.status(200).json({
+            positive: positiveArticlesUnique,
+            all: uniqueArticles
+        })
+    })
+    .catch(error => {
+        res.status(500).send(error);
+    })
+}
+
 
 module.exports={
     hello,
